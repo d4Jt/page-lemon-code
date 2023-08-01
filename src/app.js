698eaad3ce2a9e63225ddcfce2865674d50ebabe
ttD4T <jsmsj04@gmail.com>
2023-08-01 10:31:25 +0700

    Fix CORS

--- src/app.js
+++ src/app.js
@@ -4,25 +4,31 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 // init middlewares
 app.use(morgan('dev')); // morgan('combined') full thong tin
 app.use(helmet()); // bao ve thong tin rieng tu, ngan chan web thu 3 truy cap doc thong tin, cookie
 app.use(compression()); // giam dung luong van chuyen du lieu web
+let whiteList = ['http://localhost:3000', `${process.env.URL_FRONTEND}`];
+app.use(
+   cors({
+      origin: whiteList,
+      credentials: true,
+   })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 app.get('/', (req, res) => {
-   res.status(200).json("ok");
-})
+   res.status(200).json('ok');
+});
 // init db
 // require('./v1/configs/init.multi.mongodb');
 require('./v1/configs/init.mongodb');
 // require('./v1/helpers/check.connect').checkOverLoad();
-
 
 // init routes
 require('./v1/routes/index.route')(app);
