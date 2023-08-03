4fbebdcd049478b8a5acf9e788e5f3bf1327a17d
dat <jsmsj04@gmail.com>
2023-08-03 09:14:46 +0700

    Update verifyToken.js

--- src/v1/middlewares/verifyToken.js
+++ src/v1/middlewares/verifyToken.js
@@ -4,6 +4,7 @@ const {notAuth} = require('./handle_error');
 
 const verifyToken = (req, res, next) => {
     const accessToken = req?.headers['Authorization'];
+    console.log(accessToken);
     if(!accessToken) return notAuth('Access token is required', res);
     // if (!token.startsWith("Bearer")) return notAuth('Access token invalid', res);
     // const accessToken = token.split(' ')[1];
