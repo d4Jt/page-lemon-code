07cf4018d1f78aa1ac64da6e04f44f2ffec5f9fd
dat <jsmsj04@gmail.com>
2023-08-03 09:17:08 +0700

    Update verifyToken.js

--- src/v1/middlewares/verifyToken.js
+++ src/v1/middlewares/verifyToken.js
@@ -3,7 +3,7 @@ const {TokenExpiredError} = require('jsonwebtoken');
 const {notAuth} = require('./handle_error');
 
 const verifyToken = (req, res, next) => {
-    const accessToken = req?.headers['Authorization'];
+    const accessToken = req.headers["authorization"];
     console.log(accessToken);
     if(!accessToken) return notAuth('Access token is required', res);
     // if (!token.startsWith("Bearer")) return notAuth('Access token invalid', res);
