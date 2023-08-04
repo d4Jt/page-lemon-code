306ae4bbfac278f077283f4c97c4180ae3042f8a
ttD4T <jsmsj04@gmail.com>
2023-08-04 18:41:21 +0700

    Update get all tags

--- src/v1/routes/index.route.js
+++ src/v1/routes/index.route.js
@@ -3,14 +3,16 @@ const authRouter = require('./auth.router');
 const postRouter = require('./post.router');
 const userRouter = require('./user.router');
 const commentRouter = require('./comment.router');
-const {notFound} = require('../middlewares/handle_error');
+const searchRouter = require('./search.route');
+const { notFound } = require('../middlewares/handle_error');
 
 const routes = (app) => {
-    app.use('/api/auth', authRouter);
-    app.use('/api/post', postRouter);
-    app.use('/api/user', userRouter);
-    app.use('/api/comment', commentRouter);
-    app.use(notFound);
+   app.use('/api/auth', authRouter);
+   app.use('/api/post', postRouter);
+   app.use('/api/user', userRouter);
+   app.use('/api/comment', commentRouter);
+   app.use('/api/search', searchRouter);
+   app.use(notFound);
 };
 
 module.exports = routes;
